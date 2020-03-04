afc760e7c4788acdc1e4a5036862f9f6897bbf37
Branislav Nemec <bnemec@goldmann.sk>
2020-03-04 17:08:07 +0100

    some improvements

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MapInfoWindow, MapMarker, GoogleMap } from '@angular/google-maps'
 
 @Component({
   selector: 'app-map',
@@ -6,10 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+  @ViewChild(GoogleMap, { static: false }) googleMap: GoogleMap
+  @ViewChild(MapInfoWindow, { static: false }) mapInfoWindow: MapInfoWindow
+
+  zoom = 12;
+  center: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoomControl: false,
+    scrollwheel: true,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+  }
+  markers = [];
+  infoContent = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      }
+    });
+  }
+
+  mapClick(event: google.maps.MouseEvent) {
+    console.log(event.latLng.toJSON());
+    this.addMarker(event.latLng.toJSON().lat, event.latLng.toJSON().lng);
+  }
+
+  addMarker(lat: number, lng: number) {
+    this.markers.push({
+      position: {
+        lat: lat,
+        lng: lng,
+      },
+      label: {
+        color: 'red',
+        text: 'Marker label ' + (this.markers.length + 1),
+      },
+      title: 'Marker title ' + (this.markers.length + 1),
+      info: 'Marker info ' + (this.markers.length + 1),
+      options: {
+        //animation: google.maps.Animation.BOUNCE,
+      },
+    })
+  }
+
+  openInfo(marker: MapMarker, content) {
+    this.infoContent = content
+    this.mapInfoWindow.open(marker)
   }
 
 }
