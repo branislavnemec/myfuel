e0c8f4e1ee71abbe96283733a1b0d2d1c9b1c13e
Branislav Nemec <bnemec@goldmann.sk>
2020-07-29 14:33:30 +0200

    update to angular 10

--- src/app/utils/geofirex.service.ts
+++ src/app/utils/geofirex.service.ts
@@ -1,6 +1,8 @@
 import * as firebase from 'firebase/app';
 import * as geofirex from 'geofirex';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class GeoFireXService {
 
     geoFireClient: geofirex.GeoFireClient = geofirex.init(firebase);
